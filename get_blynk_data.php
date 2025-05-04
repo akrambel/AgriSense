af05a6429706c2c4664e3707c8ec279d2b4335cf
Akram bellouk <91824848+akrambel@users.noreply.github.com>
2025-05-04 21:25:26 +0100

    data_receiver.php

--- get_blynk_data.php
+++ get_blynk_data.php
@@ -1,37 +1,13 @@
 <?php
-// Blynk Auth Token
-$auth_token = "pzJxV2urte3Epw-27dtw6p4zHZ4HyfAw";
+$temperature = $_POST['temperature'];
+$humidity = $_POST['humidity'];
+$rain = $_POST['rain'];
+$soil = $_POST['soil'];
+$pressure = $_POST['pressure'];
+$altitude = $_POST['altitude'];
 
-// Virtual Pins
-$global_warming_pin = "V0";
-$humidity_pin = "V1";
-$rainfall_pin = "V2";
-$pressure_pin = "V3";
-$soil_moisture_pin = "V4";
-$temperature_pin = "V5";
-$altitude_pin = "V6";
-
-
-// Function to get data from Blynk
-function get_blynk_data($pin) {
-    global $auth_token;
-    $url = "http://blynk-cloud.com/$auth_token/get/$pin";
-    $response = file_get_contents($url);
-    return json_decode($response, true)[0];
-}
-
-// Fetch all sensor data
-$data = [
-    "humidity" => get_blynk_data($humidity_pin),
-    "temperature" => get_blynk_data($temperature_pin),
-    "pressure" => get_blynk_data($pressure_pin),
-    "altitude" => get_blynk_data($altitude_pin),
-    "soil_moisture" => get_blynk_data($soil_moisture_pin),
-    "global_warming" => get_blynk_data($global_warming_pin),
-    "rainfall" => get_blynk_data($rainfall_pin),
-];
-
-// Return JSON response
-header('Content-Type: application/json');
-echo json_encode($data);
+// Save to a file, database, etc.
+$file = fopen("sensor_data.json", "w");
+fwrite($file, json_encode($_POST));
+fclose($file);
 ?>
